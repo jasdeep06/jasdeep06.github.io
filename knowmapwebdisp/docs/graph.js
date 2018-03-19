47b56a5c4f412225ab88c5bab6309925df972766
jontyshady <prabhjotsingh040@gmail.com>
2018-03-19 14:10:01 +0530

    new css added for popup image

--- knowmapwebdisp/docs/graph.js
+++ knowmapwebdisp/docs/graph.js
@@ -280,8 +280,8 @@ function server_call(obj){
 
 }).done(function( object ) {
 
-  console.log(JSON.stringify(object));
-
+  // console.log(JSON.stringify(object));
+  $("#exploader").hide();
   init(object);
     
 });
@@ -374,7 +374,34 @@ function init(obj) {
                         //         neo4jd3.updateWithD3Data(data);
                         //         break;
                         // }
-                    },
+                       console.log(node.labels[0]);
+                       console.log(node.properties.name);
+
+
+                       var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+                      // var img = document.getElementById('myImg');
+                      var modalImg = document.getElementById("img01");
+                      // // var captionText = document.getElementById("caption");
+                      // img.onclick = function(){
+                          
+                      //     // captionText.innerHTML = this.alt;
+                      // }
+
+                      modal.style.display = "block";
+                      modalImg.src = node.properties.name;
+                      // Get the <span> element that closes the modal
+                      var span = document.getElementsByClassName("close")[0];
+
+                      // When the user clicks on <span> (x), close the modal
+                      span.onclick = function() { 
+                          modal.style.display = "none";
+                      }
+
+
+
+                  },
                     onRelationshipDoubleClick: function(relationship) {
                         console.log('double click on relationship: ' + JSON.stringify(relationship));
                     },
